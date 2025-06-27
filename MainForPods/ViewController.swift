e030622be41a1067feeef46f201a8970f8d70753
yaoyimu <yaoyimu@qq.com>
2025-06-28 02:57:46 +0800

    feat: 添加 development pods

---
+++ MainForPods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainForPods
+//
+//  Created by yym on 2025/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MainForPods-MainForPodsUITests/Pods-MainForPods-MainForPodsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MainForPods_MainForPodsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MainForPods_MainForPodsUITests
+@end
